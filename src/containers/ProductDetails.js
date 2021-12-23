815d82727e2e1f20ecaaa07cd83c60abf3796a02
neeraj kumar <neerajkmahawar2017@gmail.com>
2021-12-23 23:51:32 +0530

    Add redux thunk

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,27 +1,19 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  selectedProduct,
+  fetchProductDetail,
   removeSelectedProduct,
 } from "../redux/action/productsActions";
+
 const ProductDetails = () => {
   const { productId } = useParams();
   let product = useSelector((state) => state.product);
   const { image, title, price, category, description } = product;
   const dispatch = useDispatch();
-  const fetchProductDetail = async (id) => {
-    const response = await axios
-      .get(`https://fakestoreapi.com/products/${id}`)
-      .catch((err) => {
-        console.log("Err: ", err);
-      });
-    dispatch(selectedProduct(response.data));
-  };
 
   useEffect(() => {
-    if (productId && productId !== "") fetchProductDetail(productId);
+    if (productId && productId !== "") dispatch(fetchProductDetail(productId));
     return () => {
       dispatch(removeSelectedProduct());
     };
--- src/redux/action/productsActions.js
+++ src/redux/action/productsActions.js
@@ -1,18 +1,20 @@
+import fakeStoreApi from "../../apis/fakeStoreApi";
 import { ActionTypes } from "../constants/action-types";
 
-export const setProducts = (products) => {
-  return {
-    type: ActionTypes.SET_PRODUCTS,
-    payload: products,
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    const response = await fakeStoreApi.get("/products");
+    dispatch({ type: ActionTypes.FETCH_PRODUCTS, payload: response.data });
   };
 };
 
-export const selectedProduct = (product) => {
-  return {
-    type: ActionTypes.SELECTED_PRODUCT,
-    payload: product,
+export const fetchProductDetail = (id) => {
+  return async (dispatch) => {
+    const response = await fakeStoreApi.get(`/products/${id}`);
+    dispatch({ type: ActionTypes.SELECTED_PRODUCT, payload: response.data });
   };
 };
+
 export const removeSelectedProduct = () => {
   return {
     type: ActionTypes.REMOVE_SELECTED_PRODUCT,
